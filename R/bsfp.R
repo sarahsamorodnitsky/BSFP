aa3ae421fa3331749402350998b725290e760796
sarahsamorodnitsky <samor007@umn.edu>
2023-10-02 14:30:28 -0500

    Update plot_summaries

--- R/bsfp.R
+++ R/bsfp.R
@@ -2372,14 +2372,29 @@ summarize_factors <- function(data, Y = NULL, iters_burnin,
 #' @export
 #'
 #' @examples
-plot_summaries <- function(summaries, structure, output, source = NULL, source.name = NULL,
+plot_summaries <- function(summaries, structure, output, source = NULL, xlab.name = NULL,
                            sample.labels = NULL, biomarker.labels = NULL, label.x = FALSE) {
+
+  # summaries = output from summarize_factors
+  # structure = "joint" or "individual"
+  # output = "scores", "loadings", "betas"
+  # source = 1:q or NULL if plotting joint scores
+  # biomarker.names = c("Source 1 name", "Source 2 name", ...) e.g. c("Gene", "Metabolite", ...) or NULL if structure != "individual"
+  # grouping = label for plotting scores
+  # label.x = Boolean. Should x-axis be labeled? Could be messy with many samples/biomarkers/factors
+  # sample.labels = labels for samples if desired. NULL if output != scores or if not wanted. Default will plot CIs not containing 0.
+  # biomarker.labels = labels for biomarkers if desired. NULL if output != loadings or if not wanted. Default will plot CIs not containing 0.
 
   # Paste together structure and output names
   structure.output <- paste0(structure, ".", output)
 
   # Check if source was specified if not plotting joint scores
-  if (structure.output != "joint.scores" & is.null(source)) stop("You must specify the source argument (choose from 1:q where q is the number of sources)")
+  if (structure.output != "joint.scores" & structure.output != "joint.betas" & is.null(source)) stop("You must specify the source argument (choose from 1:q where q is the number of sources)")
+
+
+  # Check if an x-axis label was specified
+  if (structure == "loadings" & is.null(xlab.name)) stop("You must specify a name for the x-axis label using xlab.name, e.g. xlab.name = 'Gene Loadings' ")
+
 
   # Select the object from the summary
   obj.summary <- summaries[grepl(structure.output, names(summaries))][[1]]
@@ -2458,7 +2473,7 @@ plot_summaries <- function(summaries, structure, output, source = NULL, source.n
 
     # Set the x- and y-axis labels
     xlab <- "Sample"
-    xlab <- ifelse(output != "scores", ifelse(output == "loadings", source.name, "Regression Coefficients"), "Sample")
+    xlab <- ifelse(output != "scores", ifelse(output == "loadings", xlab.name, "Regression Coefficients"), "Sample")
     ylab <- paste0("Posterior Mean")
 
     # Plot the results for the scores
